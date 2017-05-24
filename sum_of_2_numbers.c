7254b5123ced9cab29084618d5c9e0c24095c554
behrad <behmardibehrad@gmail.com>
2017-05-24 17:50:28 -0400

    adding 2 number

---
+++ sum_of_2_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(int argc, char*argv[])
+
+{
+
+int first_number;
+int second_number;
+int sum=0 ;
+
+
+
+
+printf( "please enter the first number.");
+scanf("%d",&first_number); 
+//printf( "The first number = %d.",first_number);
+printf( "please enter the second number.");
+scanf("%d",&second_number); 
+//printf( "The second number = %d.",second_number);
+sum = (first_number + second_number);
+
+printf("%d + %d = %d.", first_number,second_number,sum);
+
+return 0;
+
+}
